6402909e99c277825492a0b597e4ba8974578c2d
sonegy <sonegy@sk.com>
2013-10-11 19:22:38 +0900

    String[] args 에서 변수 적용 및 Tomcat port 변경 가능

--- src/main/java/org/springcamp/boot/Application.java
+++ src/main/java/org/springcamp/boot/Application.java
@@ -1,7 +1,11 @@
 package org.springcamp.boot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -14,6 +18,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 public class Application {
+    @Value("${tomcatport:8080}")
+    private int port;
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+        return new TomcatEmbeddedServletContainerFactory(this.port);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
